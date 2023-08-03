b355c1936e023ae85fe881cbcc69f5eafdefd389
Bjorlow <dimitritorres34@gmail.com>
2023-08-03 13:33:04 -0500

    Assignment 5 launch Checklist

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+// Event listener for form submission and fetching planetary data
+document.addEventListener("DOMContentLoaded", function () {
+    // Find the form element and add an event listener for the submit event
+        // This event listener is triggered when the DOM (Document Object Model) is fully loaded.
+    // It ensures that all the elements in the HTML document are ready to be manipulated.
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    // Find the form element and add an event listener for the submit event
+    let form = document.querySelector("form");
+     // Here, we are using document.querySelector() to find the first <form> element in the HTML document and store it in the variable "form".
+    form.addEventListener("submit", function (event) {
+        // This event listener is triggered when the user submits the form.
+
+        // Prevent the default form submission behavior
+        event.preventDefault();
+        //Get the necessarry form elements 
+        const list = document.getElementById("faultyItems");
+        const pilot = document.querySelector("input[name='pilotName']");
+        const coPilot = document.querySelector("input[name='copilotName']");
+        const fuelLevel = document.querySelector("input[name='fuelLevel']");
+        const cargoLevel = document.querySelector("input[name='cargoMass']");
+         // Here, we are using document.getElementById() and document.querySelector() to select specific form elements from the HTML document and store them in variables for later use.
+        //Call the formSubmission function with correct arguments
+        formSubmission(document, list, pilot, coPilot, fuelLevel, cargoLevel);
+        // This line calls the formSubmission function, passing the necessary arguments (the selected form elements) to it.
+    });
+
+    // Fetch planetary data and update mission target
+    // This block of code fetches planetary data from an external source using the myFetch() function.
+    myFetch().then(function (result) {
+        //When the data if fetched , store it in the variable listedPlanets
+        let listedPlanets = result;
+        // We log the fetched planetary data to the console for debugging purposes.
+        console.log(listedPlanets);
+        //Pick a random planet from the fetched data
+        let planet = pickPlanet(listedPlanets);
+        // The pickPlanet() function is used to randomly select a planet from the fetched planetary data and store it in the "planet" variable.
+        //Update the mission target with information about the selected planet
+        addDestinationInfo(document, planet.name, planet.diameter, planet.star, planet.distance, planet.moons, planet.image);
+        // The addDestinationInfo() function is used to update the mission target (the element with the ID "missionTarget") with information about the selected planet.
+    });
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -1,41 +1,84 @@
 // Write your helper functions here!
 require('isomorphic-fetch');
 
+//This function is to update the mission target with informatio about the selected planet 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
-}
-
-function validateInput(testInput) {
-   
-}
-
-function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
-}
-
-async function myFetch() {
+    const missionTarget = document.getElementById('missionTarget');
+    missionTarget.innerHTML = `
+      <h2>Mission Destination</h2>
+      <ol>
+        <li>Name: ${name}</li>
+        <li>Diameter: ${diameter}</li>
+        <li>Star: ${star}</li>
+        <li>Distance from Earth: ${distance}</li>
+        <li>Number of Moons: ${moons}</li>
+      </ol>
+      <img src="${imageUrl}" alt="${name}">
+    `;
+  }
+// Function to validate input values.It checks if the input is empty or not a number.
+  function validateInput(testInput) {
+    if (testInput === "") {
+      return "Empty";
+    } else if (isNaN(testInput)) {
+      return "Not a Number";
+    } else {
+      return "Is a Number";
+    }
+  }
+//This function handles form submission and update shuttle requirements based on the input values.
+  function formSubmission(document, list, pilot, coPilot, fuelLevel, cargoLevel) {
+    // Validate the input values using the validateInput function
+    const pilotStatus = validateInput(pilot.value);
+    const coPilotStatus = validateInput(coPilot.value);
+    const fuelStatus = validateInput(fuelLevel.value);
+    const cargoStatus = validateInput(cargoLevel.value);
+  
+    // Update the list of shuttle requirements with the pilot and co-pilot names
+    list.style.visibility = "visible";
+    document.getElementById("pilotStatus").innerText = `Pilot ${pilot.value} is ready for launch`;
+    document.getElementById("copilotStatus").innerText = `Co-pilot ${coPilot.value} is ready for launch`;
+  
+    // Check fuel and cargo levels for validation
+    if (fuelStatus === "Not a Number" || cargoStatus === "Not a Number") {
+      alert("Fuel and Cargo Mass must be numbers");
+    } else if (fuelStatus === "Empty" || cargoStatus === "Empty") {
+      alert("All fields are required!");
+    } else {
+      // If all fields are valid, check fuel and cargo levels for launch readiness
+      if (fuelLevel.value < 10000) {
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("fuelStatus").innerText = "Fuel level too low for launch";
+        document.getElementById("launchStatus").innerText = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+      } else if (cargoLevel.value > 10000) {
+        document.getElementById("faultyItems").style.visibility = "visible";
+        document.getElementById("cargoStatus").innerText = "Cargo mass too high for launch";
+        document.getElementById("launchStatus").innerText = "Shuttle not ready for launch";
+        document.getElementById("launchStatus").style.color = "red";
+      } else {
+        document.getElementById("launchStatus").innerText = "Shuttle is ready for launch";
+        document.getElementById("launchStatus").style.color = "green";
+        document.getElementById("faultyItems").style.visibility = "hidden";
+      }
+    }
+  }
+//Function to fetch planetary data from URL
+  async function myFetch() {
     let planetsReturned;
-
-    planetsReturned = await fetch().then( function(response) {
-        });
-
+  
+    planetsReturned = await fetch('https://handlers.education.launchcode.org/static/planets.json').then(function(response) {
+      return response.json(); 
+    });
+  
     return planetsReturned;
-}
-
+  }
+//Function to pick a random planet from array of planets
 function pickPlanet(planets) {
-}
-
+    const randomIndex = Math.floor(Math.random() * planets.length);
+    return planets[randomIndex];
+  }
+//Export the helper functions to be used in the main script.js file
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
